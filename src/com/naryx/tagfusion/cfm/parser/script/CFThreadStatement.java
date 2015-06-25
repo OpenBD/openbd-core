d6d8a1d56f171451acafe45da169adb47a8d48a8
Alan Williamson <github@alan.is>
2015-06-25 12:25:01 -0400

    fixed thread() script null pointer

--- src/com/naryx/tagfusion/cfm/parser/script/CFThreadStatement.java
+++ src/com/naryx/tagfusion/cfm/parser/script/CFThreadStatement.java
@@ -1,29 +1,29 @@
-/* 
+/*
  *  Copyright (C) 2000 - 2010 TagServlet Ltd
  *
  *  This file is part of Open BlueDragon (OpenBD) CFML Server Engine.
- *  
+ *
  *  OpenBD is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  Free Software Foundation,version 3.
- *  
+ *
  *  OpenBD is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with OpenBD.  If not, see http://www.gnu.org/licenses/
- *  
+ *
  *  Additional permission under GNU GPL version 3 section 7
- *  
- *  If you modify this Program, or any covered work, by linking or combining 
- *  it with any of the JARS listed in the README.txt (or a modified version of 
- *  (that library), containing parts covered by the terms of that JAR, the 
- *  licensors of this Program grant you additional permission to convey the 
- *  resulting work. 
+ *
+ *  If you modify this Program, or any covered work, by linking or combining
+ *  it with any of the JARS listed in the README.txt (or a modified version of
+ *  (that library), containing parts covered by the terms of that JAR, the
+ *  licensors of this Program grant you additional permission to convey the
+ *  resulting work.
  *  README.txt @ http://www.openbluedragon.org/license/README.txt
- *  
+ *
  *  http://www.openbluedragon.org/
  */
 package com.naryx.tagfusion.cfm.parser.script;
@@ -56,7 +56,7 @@ public class CFThreadStatement extends CFParsedAttributeStatement implements Ser
 
 	private static final long serialVersionUID = 1L;
 
-	private CFScriptStatement body;
+	private final CFScriptStatement body;
 
 	private static HashSet<String> supportedAttributes;
 
@@ -118,7 +118,9 @@ public class CFThreadStatement extends CFParsedAttributeStatement implements Ser
 	@Override
 	public void setHostTag( cfTag _parentTag ) {
 		super.setHostTag( _parentTag );
-		body.setHostTag( _parentTag );
+
+		if ( body != null )
+			body.setHostTag( _parentTag );
 	}
 
 
