a55b84fffc25d44a8d795bad1a08aa0c4d5ed11a
ahuggett <amanda@aw20.co.uk>
2017-11-16 10:34:40 +0000

    Review Comments applied

--- src/org/alanwilliamson/amazon/lambda/LambdaExecute.java
+++ src/org/alanwilliamson/amazon/lambda/LambdaExecute.java
@@ -1,3 +1,32 @@
+/* 
+ *  Copyright (C) 2000 - 2015 aw2.0Ltd
+ *
+ *  This file is part of Open BlueDragon (OpenBD) CFML Server Engine.
+ *  
+ *  OpenBD is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  Free Software Foundation,version 3.
+ *  
+ *  OpenBD is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenBD.  If not, see http://www.gnu.org/licenses/
+ *  
+ *  Additional permission under GNU GPL version 3 section 7
+ *  
+ *  If you modify this Program, or any covered work, by linking or combining 
+ *  it with any of the JARS listed in the README.txt (or a modified version of 
+ *  (that library), containing parts covered by the terms of that JAR, the 
+ *  licensors of this Program grant you additional permission to convey the 
+ *  resulting work. 
+ *  README.txt @ http://www.openbluedragon.org/license/README.txt
+ *  
+ *  http://openbd.org/
+ *  $Id$
+ */
 package org.alanwilliamson.amazon.lambda;
 
 import java.nio.ByteBuffer;
@@ -32,72 +61,80 @@ public class LambdaExecute extends AmazonBase {
 
 	private static final long serialVersionUID = 1L;
 
-	public LambdaExecute(){  min = 3; max = 4; setNamedParams( new String[]{ "datasource", "function","payload", "qualifier" } ); }
+
+	public LambdaExecute() {
+		min = 3;
+		max = 4;
+		setNamedParams( new String[] { "datasource", "function", "payload", "qualifier" } );
+	}
+
 
 	@Override
-	public String[] getParamInfo(){
-		return new String[]{
-			"Lambda Session",
-			"Lambda Function Name",
-			"JSON Payload for the Lambda Function",
-			"Lambda Function Alias"
+	public String[] getParamInfo() {
+		return new String[] {
+				"Lambda Session",
+				"Lambda Function Name",
+				"JSON Payload for the Lambda Function",
+				"Lambda Function Alias"
 		};
 	}
-  
+
+
 	@Override
-	public java.util.Map<String,String> getInfo(){
+	public java.util.Map<String, String> getInfo() {
 		return makeInfo(
-				"amazon", 
-				"Amazon Lambda: executes a lambda function", 
+				"amazon",
+				"Amazon Lambda: executes a lambda function",
 				ReturnType.STRUCTURE );
 	}
-	
+
+
 	/**
 	 * Executes a lambda function and returns the result of the execution.
 	 */
 	@Override
 	public cfData execute( cfSession _session, cfArgStructData argStruct ) throws cfmRunTimeException {
-		
-		AmazonKey amazonKey	= getAmazonKey(_session, argStruct);
+
+		AmazonKey amazonKey = getAmazonKey( _session, argStruct );
 
 		// Arguments to extract
-		String payload = getNamedStringParam(argStruct, "payload", null );
-		String functionName = getNamedStringParam(argStruct, "function", null );
-		String qualifier = getNamedStringParam(argStruct, "qualifier", null);
-		
+		String payload = getNamedStringParam( argStruct, "payload", null );
+		String functionName = getNamedStringParam( argStruct, "function", null );
+		String qualifier = getNamedStringParam( argStruct, "qualifier", null );
+
 		try {
 
 			// Construct the Lambda Client
 			InvokeRequest invokeRequest = new InvokeRequest();
-	    	invokeRequest.setInvocationType( InvocationType.RequestResponse ); 
-	    	invokeRequest.setLogType( LogType.Tail );
-	    	invokeRequest.setFunctionName( functionName );
-	    	invokeRequest.setPayload( payload );
-	    	if ( qualifier != null ) {
-		    	invokeRequest.setQualifier(qualifier);	    		
-	    	}
-	    	
-	    	// Lambda client must be created with credentials
-			BasicAWSCredentials awsCreds = new BasicAWSCredentials(amazonKey.getKey(), amazonKey.getSecret());
+			invokeRequest.setInvocationType( InvocationType.RequestResponse );
+			invokeRequest.setLogType( LogType.Tail );
+			invokeRequest.setFunctionName( functionName );
+			invokeRequest.setPayload( payload );
+			if ( qualifier != null ) {
+				invokeRequest.setQualifier( qualifier );
+			}
+
+			// Lambda client must be created with credentials
+			BasicAWSCredentials awsCreds = new BasicAWSCredentials( amazonKey.getKey(), amazonKey.getSecret() );
 			AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
 					.withRegion( amazonKey.getAmazonRegion().toAWSRegion().getName() )
-	                .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+					.withCredentials( new AWSStaticCredentialsProvider( awsCreds ) ).build();
 
 			// Execute and process the results
-	    	InvokeResult result = awsLambda.invoke( invokeRequest );
-			
-	    	// Convert the returned result
-	    	ByteBuffer resultPayload = result.getPayload();
-	    	String resultJson = new String(resultPayload.array());
-	    	Map <String, Object> resultMap = Jackson.fromJsonString( resultJson, Map.class);
+			InvokeResult result = awsLambda.invoke( invokeRequest );
+
+			// Convert the returned result
+			ByteBuffer resultPayload = result.getPayload();
+			String resultJson = new String( resultPayload.array(), "UTF-8" );
+			Map<String, Object> resultMap = Jackson.fromJsonString( resultJson, Map.class );
 
 			return tagUtils.convertToCfData( resultMap );
 
 		} catch ( Exception e ) {
 			throwException( _session, "AmazonLambdaExecute: " + e.getMessage() );
-			return cfBooleanData.FALSE;	
+			return cfBooleanData.FALSE;
 		}
-    	
+
 	}
 
 }
