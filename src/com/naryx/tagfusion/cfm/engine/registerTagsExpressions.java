9b75cfd2151477830a337b96f7e659109a18e431
Fernstrom, Marcus <mfernstrom@eab.com>
2018-05-11 10:44:01 -0400

    Added PBKDF2 hashing as a new function, as it's quite different from the existing CFML hashing, and I didn't want to break backward compatibility.

--- src/com/naryx/tagfusion/cfm/engine/registerTagsExpressions.java
+++ src/com/naryx/tagfusion/cfm/engine/registerTagsExpressions.java
@@ -276,6 +276,7 @@ public class registerTagsExpressions extends java.lang.Object {
     functions.put( "getfunctionlist",	     "com.naryx.tagfusion.expression.function.getFunctionList" );
     functions.put( "gettimezoneinfo",	     "com.naryx.tagfusion.expression.function.getTimeZoneInfo" );
     functions.put( "hash",	               "com.naryx.tagfusion.expression.function.hash" );
+    functions.put( "pbkdf2",	               "com.naryx.tagfusion.expression.function.pbkdf2" );
     functions.put( "structappend",	       "com.naryx.tagfusion.expression.function.struct.structAppend" );
     functions.put( "structfindkey",	       "com.naryx.tagfusion.expression.function.struct.structFindKey" );
     functions.put( "getcurrenttemplatepath","com.naryx.tagfusion.expression.function.getCurrentTemplatePath" );
